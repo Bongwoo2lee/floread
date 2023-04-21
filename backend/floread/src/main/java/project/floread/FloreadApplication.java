26062153eee2db113994c76a869da0625a817106
stoneTiger0912 <seokbeom0912@gmail.com>
2023-04-22 00:44:19 +0900

    Add backend_file

---
+++ backend/floread/src/main/java/project/floread/FloreadApplication.java
@@ -0,0 +1,13 @@
+package project.floread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloreadApplication.class, args);
+	}
+
+}
