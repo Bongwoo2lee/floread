18c7d93193f19e5ebeaad0503b1663d711174ce6
jinbong <wlftj13@naver.com>
2023-04-30 16:14:32 +0900

    수정

--- svelte-start-app/src/Floread.svelte
+++ svelte-start-app/src/Floread.svelte
@@ -1,9 +1,11 @@
 <script>
-	let src='https://github.com/Bongwoo2lee/floread/blob/frontend/svelte-start-app/src/1166091_ORSJPI0.jpg';
+	let src="https://raw.githubusercontent.com/Bongwoo2lee/floread/frontend/svelte-start-app/src/1166091_ORSJPI0.jpg";
+	let src2="https://raw.githubusercontent.com/Bongwoo2lee/floread/frontend/svelte-start-app/src/5542689_2866296.jpg"
+	let src3="https://raw.githubusercontent.com/Bongwoo2lee/floread/frontend/svelte-start-app/src/5567275_2901224.jpg"
 </script>
 <style>
     p{
-        color: rgb(195, 184, 173);
+        color: rgb(255, 244,233);
         font-family: 'Georgia', serif;
         font-size: 1em;
         text-align: center;
@@ -26,17 +28,40 @@
 		rgba(0,0,0,0.8)95%,
 		rgba(0,0,0,1)100%
 		),
-		url("https://github.com/Bongwoo2lee/floread/blob/frontend/svelte-start-app/src/1511831767380871.jpg");
+		url("https://raw.githubusercontent.com/Bongwoo2lee/floread/frontend/svelte-start-app/src/1511831767380871.jpg");
 		background-repeat: no-repeat;
 		background-size: 100%;
 	}
+	#wrap{
+		margin: 0 auto;
+		overflow: hidden;
+		padding-top: 10px;
+	}
+	div>article{
+		float: left;
+		margin-left: 225px;
+		margin-bottom: 150px;
+	}
+	img{display: block;}
 </style>
 <body bgcolor="black">
 	<div>
     <p style="line-height: 0px; font-size:6.5em;">Floread</p>
     <p style="line-height: 0px;">Music stuck on the bookshelf </p>
 </div>
-<div>
-	<img src={src} alt='img'>
+<div style=" width:95%; margin:auto;">
+	<p style="color:rgba(255, 244,233);font-size: 3em;font-family:Gothic A1, san-serif;"><b>당신의 독서를 돕는 음악과 함께 하세요</b></p>
+<table style="width: 100%; text-align:center; margin:auto;">
+	<tr>
+		<td><img style="margin: auto;" src={src} alt="이미지" width='150px' height='150px'></td>
+		<td><img style="margin: auto;" src={src2} alt="이미지" width='150px' height='150px'></td>
+		<td><img style="margin: auto;" src={src3} alt="이미지" width='150px' height='150px'></td>		
+	</tr>
+	<tr>
+		<td><p>UPLOAD</p></td>
+		<td><p>VIEWER</p></td>
+		<td><p>MYPAGE</p></td>		
+	</tr>
+</table>
 </div>
 </body>
