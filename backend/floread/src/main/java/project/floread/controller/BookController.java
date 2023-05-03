63ba800209e0a27c5cb971a9f1d3aa6845a0cff1
stoneTiger0912 <seokbeom0912@gmail.com>
2023-05-04 00:35:40 +0900

    Add Upload_frontend(tmp) and Postmapping

--- backend/floread/src/main/java/project/floread/controller/BookController.java
+++ backend/floread/src/main/java/project/floread/controller/BookController.java
@@ -21,8 +21,6 @@ import project.floread.service.BookService;
 import project.floread.service.EmotionService;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
@@ -36,58 +34,64 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping("/books/save")
-    public String create(/*@RequestPart MultipartFile files, */ Authentication authentication) throws IOException {
+    public String upload() {
+        return "books-save";
+    }
+
+
+    @PostMapping("/books/save")
+    public String create(@RequestPart("file") MultipartFile[] files,  Authentication authentication) throws IOException {
+/*음*/
 
         //현재 로그인 중인 유저 userId가져오기
         String userId = authentication.getName();
+        for (MultipartFile file : files) {
+            if(file.isEmpty()) {
+                System.out.println("파일 없음");
+                return "index";
+            }
+            try {
+                Book book = new Book();
+                //원본 파일 이름 저장
+                String sourceFileName = file.getOriginalFilename();
+                //원본파일확장자면
+                String sourceFileNameExtension = FilenameUtils.getExtension(sourceFileName);
+                //확장자빼고
+                String title = FilenameUtils.removeExtension(sourceFileName);
 
-        Book book = new Book();
-        //업로드 이벤트 없이 그냥 있는 텍스트 파일로 하는 법
-        File file = new File("/Users/seokbeomlee/Desktop/Project/floread/backend/floread/src/main/java/project/floread/controller/개구리 왕자.txt");
-        DiskFileItem fileItem = new DiskFileItem("file", Files.probeContentType(file.toPath()), false, file.getName(), (int) file.length(), file.getParentFile());
-        InputStream input = new FileInputStream(file);
-        OutputStream os = fileItem.getOutputStream();
-        IOUtils.copy(input, os);
+                //저장될 파일
+                File destinationBook;
+                String destinationBookName;
+                String bookUrl = "/Users/seokbeomlee/Desktop/Project/floread/book/";
+                try {
 
-        MultipartFile files = new CommonsMultipartFile(fileItem);
-        //여기까지
+                    //파일명은 사용자auth2아이디_원본파일.txt로 저장됨
+                    destinationBookName = title + '_' + userId + '.' + sourceFileNameExtension;
 
-        //원본 파일 이름 저장
-        String sourceFileName = files.getOriginalFilename();
+                    //파일 경로
+                    destinationBook = new File(bookUrl + destinationBookName);
 
-        //원본파일확장자면
-        String sourceFileNameExtension = FilenameUtils.getExtension(sourceFileName);
+                    //부모디렉토리가 존재하지 않으면 생성
+                    destinationBook.getParentFile().mkdirs();
+                    //파일 이동
+                    file.transferTo(destinationBook);
 
-        //확장자빼고
-        String title = FilenameUtils.removeExtension(sourceFileName);
+                    //데이터베이스에 저장
+                    book.setFileName(destinationBookName);
+                    book.setUrl(bookUrl + destinationBookName);
+                    bookService.join(book, userId);
 
-        //저장될 파일
-        File destinationBook;
-        String destinationBookName;
-        String bookUrl = "/Users/seokbeomlee/Desktop/Project/floread/book/";
-        //있을 경우 패스
-        try {
-            do {
-                //파일명은 사용자auth2아이디_원본파일.txt로 저장됨
-                destinationBookName = title + '_' + userId + '.' + sourceFileNameExtension;
+                } catch (IllegalStateException e) {
+                    System.out.println("파일 존재");
+                }
 
-                //파일 경로
-                destinationBook = new File(bookUrl + destinationBookName);
-
-                //부모디렉토리가 존재하지 않으면 생성
-                destinationBook.getParentFile().mkdirs();
-                //파일 이동
-                files.transferTo(destinationBook);
-
-                //데이터베이스에 저장
-                book.setFileName(destinationBookName);
-                book.setUrl(bookUrl + destinationBookName);
-                bookService.join(book, userId);
-            } while (destinationBook.exists());
-        } catch (IllegalStateException e) {
-            return "books-save";
+            } catch (IOException e) {
+                System.out.println("저장 실패");
+                return "index";
+            }
         }
-        return "books-save";
+        return "index";
+        //있을 경우 패스
     }
 
 
