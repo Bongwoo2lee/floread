9135b0fa2c4822de700eeb6f3db09d5ee8c82f6e
jinbong <wlftj13@naver.com>
2023-09-01 17:49:14 +0900

    -

--- frontend/todo-react-app/src/App.js
+++ frontend/todo-react-app/src/App.js
@@ -1,7 +1,32 @@
-import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 function App() {
-  return <div>hello</div>;
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const toggle = () => {
+    setLoggedIn(!loggedIn);
+  };
+
+  const handleLoginClick = () => {
+    toggle();
+    window.open('http://floread.store:8000', '_blank', 'width=500,height=500');
+  };
+
+  return (
+    <div style={{ backgroundColor: 'black' }}>
+      <div style={{ float: 'right' }}>
+        <button
+          style={{ background: 'none' }}
+          onClick={handleLoginClick}
+        >
+          <p style={{ lineHeight: 'normal' }}>log in</p>
+        </button>
+      </div>
+      <div>
+        <p style={{ lineHeight: '0px', fontSize: '6.5em' }}>Floread</p>
+        <p style={{ lineHeight: '0px' }}>Music stuck on the bookshelf</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
